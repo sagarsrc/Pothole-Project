0bd2daddca754662f10c9e2c515959e87aca916a
Sagar Sarkale <sagxam@gmail.com>
2019-01-31 23:47:05 +0530

    Labelling complete
    - Labelling for unmarked speed breakers done

--- labelling/labels.py
+++ labelling/labels.py
@@ -3,6 +3,7 @@ __author__ = "HexaByte"
 '''
 
 import pandas as pd
+import numpy as np
 
 class Labels(object):
 	"""Class for creating Labels"""
@@ -59,3 +60,167 @@ class Labels(object):
 			})
 		
 		return df_res
+
+
+
+	def set_vertical_limits(self,df):
+		"""
+		Get vertical limits for downsampled dataframe
+		args:
+			df : downsampled dataframe with columns(event,value)
+		return:
+			vlimits : DataFrame starting point of vertical barrier as event column
+		"""
+		ev = df.copy(deep=True)
+		dif = ev['event'].diff()
+		dif = dif[1:]
+		mean_window_size = dif.mean()
+
+		print("Window size : ",mean_window_size)
+		
+		s = 0
+		l = []
+		for x,i in enumerate(dif,1):
+			# print(x,i)
+			s+=i
+			if s >= mean_window_size:
+				l.append(x)
+				s=0
+		 #print(l)
+		return ev.iloc[l]
+
+
+
+		
+	def set_horizontal_limits(self,df):
+		"""
+		Get horizontal limits for downsampled dataframe
+		args:
+			df : downsampled dataframe with columns(event,value)
+		return:
+			hlimits : tuples of (h_up,h_dn)
+		"""
+		hlimits_val = df['value'].mean()
+		print("hlimit_val (mean of all 'value' in downsampled points) = ",hlimits_val)
+
+		print("zero =",df.loc[0,'value'])
+
+		# set h_up and h_dn
+		h_up = df.loc[0,'value']+hlimits_val
+		h_dn = np.absolute(df.loc[0,'value']-hlimits_val)
+
+		# tuple hlimits
+		hlimits = (h_up,h_dn)
+		print("h_up,h_dn = ",hlimits)
+		
+		return hlimits
+
+
+
+
+	def get_thold_pts(self,df,hlimits):
+		"""
+		Get dataframe of all points which have crossed threshold h_up and h_dn
+		args:
+			df : DataFrame with event and value columns (Downsampled dataframe)
+			hlimits : tuple (h_up,h_dn)
+		return:
+			thold_pts : dataframe 
+		"""
+		h_up,h_dn = hlimits
+		h_up_points = df[df['value']>h_up]
+		h_dn_points = df[df['value']<h_dn]
+		thold_pts =h_up_points.append(h_dn_points,ignore_index=False)
+		
+		return thold_pts
+
+
+
+	def get_mean_thold(self,df,thold_pts):
+		"""
+		Get mean threshold percentage to identify window to label
+		args:
+			df: downsampled dataframe
+			thold_pts : dataframe of all points which have crossed threshold h_up and h_dn
+		returns:
+			mean threshold percentage
+		"""
+		ix = thold_pts.index.values
+		
+		l = []
+		
+		for i in range(ix.shape[0]-1):
+			# print(ix[i],ix[i+1])
+			temp_vals = df.iloc[ix[i]:ix[i+1],:]
+			# print(temp_vals)
+
+			# print(temp_vals.isin(thold_pts))
+			win_tf = temp_vals.isin(thold_pts)
+
+			# print(win_tf['value'].value_counts())
+			try:
+				f,t = win_tf['value'].value_counts(sort=False)
+			except:
+				t = 0
+			v = (t/(t+f))*100
+
+			l.append(v)
+
+		mean_thold_percent = np.array(l).mean()
+		print("Mean threshold % ",mean_thold_percent)
+		return mean_thold_percent
+
+
+
+
+	def get_labels(self,df,thold_pts,label=1):
+		"""
+		get labels for downsampled data
+		args:
+			df : dataframe of downsampled points
+			thold_pts : dataframe of points which have crossed h_up,h_dn
+		returns:
+			labels to downsampled dataframe
+		"""
+		
+		labels_df = pd.DataFrame(columns=['event','value','label'])
+		s = 0
+		# call get_mean_thold
+		mean_thold_percent = self.get_mean_thold(df,thold_pts)
+		
+		ix = thold_pts.index.values
+
+		for i in range(ix.shape[0]-1):
+			# print(ix[i],ix[i+1])
+			temp_vals = df.iloc[ix[i]:ix[i+1],:]
+
+			win_tf = temp_vals.isin(thold_pts)
+
+			try:
+				f,t = win_tf['value'].value_counts(sort=False)
+			except:
+				t = 0
+			v = (t/(t+f))*100
+
+			if v>mean_thold_percent:
+				# s+=temp_vals.shape[0]
+				# print("True",s)
+				ev = temp_vals['event']
+				vl = temp_vals['value']
+				lb = np.ones(temp_vals.shape[0])
+
+				temp_df = pd.DataFrame({
+					'event':ev,
+					'value':vl,
+					'label':lb
+				})
+
+				labels_df = pd.concat([labels_df,temp_df])
+
+		labels_df.head()       
+
+		df['label'] = np.zeros(df.shape[0])
+		df.head()
+
+		df.iloc[labels_df.index,-1] = label
+		return df
--- labelling/verify_labels.py
+++ labelling/verify_labels.py
@@ -117,3 +117,173 @@ class VerifyLabels(object):
 		fig = go.Figure(data=data)
 
 		return iplot(fig)
+
+
+	def verify_horizontal_limits(self,df,col_name, ev_vals_df,sample_index,vlimits,hlimits):
+		"""
+		Verify horizontal limits
+		args:
+			df : pandas DataFrame with columns (Timestamp,Gx,Gy,Gz,Ax,Ay,Az)
+			col_name : column which needs to be verified
+			ev_vals_df : event and value dataframe consisting downsampled 
+			sample_index : index values of samples (int __NOT datetime__)
+			
+			vlimits : dataframe of vertical limits with event and values
+			hlimits : (tuple) horizontal limits 
+
+		returns:
+			plot of samples and downsampled points
+		"""
+		t = df['Time']
+		y_whole = df[col_name]
+		# print(t)
+		Signal = go.Scatter(
+			x= t,
+			y= y_whole,
+			name='signal',
+			mode='lines'
+		)
+
+		result = t.searchsorted(value = sample_index)
+		# print(result)
+		# print(df.iloc[result])
+		sample_df = df.iloc[result]
+
+		Samples = go.Scatter(
+			x = sample_df['Time'],
+			y= sample_df[col_name],
+			name='samples',
+			mode='markers'
+		)
+
+		vlims_time_index = t.searchsorted(value=vlimits['event'])
+		# print(vlims_time_index)
+		# print(df.iloc[vlims_time_index])
+		vlims_df = df.iloc[vlims_time_index]
+
+		limits = go.Bar(
+
+			x = vlims_df['Time'],
+			# 60. is height of the barrier
+			y = np.array([60.]*vlims_df.shape[0]),
+			name='Vertical Barrier',
+
+			width=0.0002,
+			marker=dict(
+				color='rgb(255,0,0)',
+			)
+
+		)
+		
+		hor_bars = go.Scatter(
+			x = df['Time'],
+			y = np.array([hlimits[0]]*df.shape[0]),
+			mode = 'lines',
+			name='h_up'
+		)
+		
+		lor_bars = go.Scatter(
+			x = df['Time'],
+			y = np.array([hlimits[1]]*df.shape[0]),
+			mode = 'lines',
+			name='h_dn'
+		)
+		
+
+		data = [Signal,Samples,limits,hor_bars,lor_bars]
+		fig = go.Figure(data=data)
+
+		return iplot(fig)
+
+
+
+	def verify_labels(self,df,col_name, labelled_ev_vals_df,sample_index,vlimits,hlimits):
+		"""
+		Verify labels
+		args:
+			df : pandas DataFrame with columns (Timestamp,Gx,Gy,Gz,Ax,Ay,Az)
+			col_name : column which needs to be verified
+			ev_vals_df : event and value dataframe consisting downsampled 
+			sample_index : downsampled data index values of samples (float __NOT datetime__)
+
+			vlimits : dataframe of vertical limits with event and values
+			hlimits : (tuple) horizontal limits 
+
+		returns:
+			plot of samples and downsampled points
+		"""
+		t = df['Time']
+		y_whole = df[col_name]
+		# print(t)
+		Signal = go.Scatter(
+			x= t,
+			y= y_whole,
+			name='signal',
+			mode='lines'
+		)
+
+		result = t.searchsorted(value = sample_index)
+		# print(result)
+		# print(df.iloc[result])
+		# sample_df = df.iloc[result]
+
+		label1X = labelled_ev_vals_df[labelled_ev_vals_df['label']==1]['event']
+		label1Y = labelled_ev_vals_df[labelled_ev_vals_df['label']==1]['value']
+		
+		label0X = labelled_ev_vals_df[labelled_ev_vals_df['label']==0]['event']
+		label0Y = labelled_ev_vals_df[labelled_ev_vals_df['label']==0]['value']
+		
+		
+		lb1 = go.Scatter(
+			x = label1X,
+			y = label1Y,
+			name = 'lb1',
+			mode = 'markers'
+		)
+		
+		lb0 = go.Scatter(
+			x = label0X,
+			y = label0Y,
+			name = 'lb0',
+			mode = 'markers'
+		)
+
+		vlims_time_index = t.searchsorted(value=vlimits['event'])
+		# print(vlims_time_index)
+		# print(df.iloc[vlims_time_index])
+		vlims_df = df.iloc[vlims_time_index]
+
+		limits = go.Bar(
+
+			x = vlims_df['Time'],
+			# 60. is height of the barrier
+			y = np.array([60.]*vlims_df.shape[0]),
+			name='Vertical Barrier',
+
+			width=0.0002,
+			marker=dict(
+				color='rgb(255,0,0)',
+			)
+
+		)
+
+		hor_bars = go.Scatter(
+			x = df['Time'],
+			y = np.array([hlimits[0]]*df.shape[0]),
+			mode = 'lines',
+			name='h_up'
+		)
+
+		lor_bars = go.Scatter(
+			x = df['Time'],
+			y = np.array([hlimits[1]]*df.shape[0]),
+			mode = 'lines',
+			name='h_dn'
+		)
+		
+		
+
+		data = [Signal,lb0,lb1,limits,hor_bars,lor_bars]
+		fig = go.Figure(data=data)
+
+		return iplot(fig)
